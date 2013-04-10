2c7cf9b8d57a3c622d907dbc99b4dd1a3d95000b
Hanif Jameel <hanif_1989@btinternet.com>
2013-04-10 22:59:45 +0100

    calculate a strike

--- bowlinggamekata/bowlinggame.py
+++ bowlinggamekata/bowlinggame.py
@@ -11,11 +11,16 @@ class Game(object):
         score = 0
         frame_index = 0
         for frame in range(10):
-            if self._is_spare(frame_index):
+            if self.rolls[frame_index] == 10:
+                score += 10 + \
+                        self.rolls[frame_index+1] + self.rolls[frame_index+2]
+                frame_index += 1
+            elif self._is_spare(frame_index):
                 score += 10 + self.rolls[frame_index+2]
+                frame_index += 2
             else:
                 score += self.rolls[frame_index] + self.rolls[frame_index+1]
-            frame_index += 2
+                frame_index += 2
         return score
 
     def _is_spare(self, frame_index):
