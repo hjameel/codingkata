c26c3cbaa93abd123794950d7b44d5255604cee6
Hanif Jameel <hanif_1989@btinternet.com>
2013-04-10 22:18:29 +0100

    a separate method to better name how to roll a spare

--- bowlinggamekata/bowlinggame/test/bowlinggametest.py
+++ bowlinggamekata/bowlinggame/test/bowlinggametest.py
@@ -25,12 +25,15 @@ class BowlingGameTest(unittest.TestCase):
         self.assertEquals(20, self.g.score())
 
     def test_one_spare(self):
-        self.g.roll(5)
-        self.g.roll(5) #spare
+        self._roll_spare()
         self.g.roll(3)
         self._roll_many(17, 0)
 
         self.assertEquals(16, self.g.score())
+
+    def _roll_spare(self):
+        self.g.roll(5)
+        self.g.roll(5)
 
 
 if __name__ == "__main__":
