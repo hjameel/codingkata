7b7f3ba69ab747d1fc2981c1aa3f8731b381a453
Hanif Jameel <hanif_1989@btinternet.com>
2013-04-10 21:54:45 +0100

    removed duplication of roll loop in tests

--- bowlinggamekata/bowlinggame/test/bowlinggametest.py
+++ bowlinggamekata/bowlinggame/test/bowlinggametest.py
@@ -12,15 +12,18 @@ class BowlingGameTest(unittest.TestCase):
     def setUp(self):
         self.g = Game()
 
+    def _roll_many(self, n, pins):
+        for i in range(n):
+            self.g.roll(pins);
+
     def test_gutter_game(self):
-        for i in range(20):
-            self.g.roll(0);
+        self._roll_many(20, 0)
         self.assertEquals(0, self.g.score())
 
     def test_all_ones(self):
-        for i in range(20):
-            self.g.roll(1)
+        self._roll_many(20, 1)
         self.assertEquals(20, self.g.score())
+
 
 if __name__ == "__main__":
     unittest.main()
