ab23ec89fdb989d3cc797378fb37e1638146ce6e
Hanif Jameel <hanif_1989@btinternet.com>
2013-04-10 21:48:34 +0100

    can count a game of all ones

--- bowlinggamekata/bowlinggame/bowlinggame.py
+++ bowlinggamekata/bowlinggame/bowlinggame.py
@@ -1,6 +1,9 @@
 class Game(object):
+    def __init__(self):
+        self.score_counter = 0
+
     def roll(self, pins):
-        pass
+        self.score_counter += pins
 
     def score(self):
-        return 0
+        return self.score_counter
