fc4b76e6d8de96f9b53ee06d487b166d98e468ea
Hanif Jameel <hanif_1989@btinternet.com>
2013-04-10 21:41:42 +0100

    first test

--- bowlinggamekata/bowlinggame/test/bowlinggametest.py
+++ bowlinggamekata/bowlinggame/test/bowlinggametest.py
@@ -1,5 +1,16 @@
+import os.path
+import sys
 import unittest
 
-class BowlingGameTest(unittest.TestCase):
-    pass
+# Add project root to module search path
+project_root = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..")
+sys.path.insert(0, project_root)
 
+from bowlinggame import Game
+
+class BowlingGameTest(unittest.TestCase):
+    def test_gutter_game(self):
+        g = Game()
+
+if __name__ == "__main__":
+    unittest.main()
