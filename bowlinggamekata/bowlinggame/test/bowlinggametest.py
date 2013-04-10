6fe849fbffa89650d6cbaba9aa3515264aae7c3f
Hanif Jameel <hanif_1989@btinternet.com>
2013-04-10 21:44:39 +0100

    passing gutter game test

--- bowlinggamekata/bowlinggame/test/bowlinggametest.py
+++ bowlinggamekata/bowlinggame/test/bowlinggametest.py
@@ -13,6 +13,7 @@ class BowlingGameTest(unittest.TestCase):
         g = Game()
         for i in range(20):
             g.roll(0);
+        self.assertEquals(0, g.score())
 
 if __name__ == "__main__":
     unittest.main()
