ddf00b5e0db98e73db57d11ad7adfdacbc8a29d1
Hanif Jameel <hanif_1989@btinternet.com>
2013-04-10 23:00:25 +0100

    Remove unnecessary code

--- bowlinggamekata/bowlinggametest.py
+++ bowlinggamekata/bowlinggametest.py
@@ -1,10 +1,4 @@
-import os.path
-import sys
 import unittest
-
-# Add project root to module search path
-project_root = os.path.join(os.path.abspath(os.path.dirname(__file__)), "..")
-sys.path.insert(0, project_root)
 
 from bowlinggame import Game
 
