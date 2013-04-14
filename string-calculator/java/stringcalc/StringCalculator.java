7068bf64ab2d63712a10fb05037941a514d81db5
Hanif Jameel <hanif_1989@btinternet.com>
2013-04-14 21:55:07 +0100

    throw exception if there is a negative number + code reformatting

--- string-calculator/java/stringcalc/StringCalculator.java
+++ string-calculator/java/stringcalc/StringCalculator.java
@@ -1,49 +1,53 @@
 package stringcalc;
 
 public class StringCalculator {
-	
-	private static final String DEFAULT_DELIMITER = "[,\n]";
-	private static final String CUSTOM_DELIMITER_PATTERN = "//.\n.*";
-	private static final int CUSTOM_DELIM_INDEX = 2;
-	private static final int CUSTOM_DELIMITED_STRING_INDEX = 4;
 
-	public int add(String s) {
-		if (s.isEmpty())
-			return 0;
+    private static final String DEFAULT_DELIMITER = "[,\n]";
+    private static final String CUSTOM_DELIMITER_PATTERN = "//.\n.*";
+    private static final int CUSTOM_DELIM_INDEX = 2;
+    private static final int CUSTOM_DELIMITED_STRING_INDEX = 4;
 
-		return sumOfNumbersInString(s);
-	}
+    public int add(String s) throws Exception {
+        if (s.isEmpty())
+            return 0;
 
-	private int sumOfNumbersInString(String s) {
-		String delimiter = DEFAULT_DELIMITER;
-		if (hasCustomDelimiter(s)) {
-			delimiter = getCustomDelimiter(s);
-			s = removeDelimiterSpecifierFromString(s);
-		}
-		
-		return sumNumbersInDelimitedString(s, delimiter);
-	}
+        return sumOfNumbersInString(s);
+    }
 
-	private boolean hasCustomDelimiter(String s) {
-		return s.matches(CUSTOM_DELIMITER_PATTERN);
-	}
+    private int sumOfNumbersInString(String s) throws Exception {
+        String delimiter = DEFAULT_DELIMITER;
+        if (hasCustomDelimiter(s)) {
+            delimiter = getCustomDelimiter(s);
+            s = removeDelimiterSpecifierFromString(s);
+        }
 
-	private String removeDelimiterSpecifierFromString(String s) {
-		s = s.substring(CUSTOM_DELIMITED_STRING_INDEX);
-		return s;
-	}
+        return sumNumbersInDelimitedString(s, delimiter);
+    }
 
-	private String getCustomDelimiter(String s) {
-		return s.substring(CUSTOM_DELIM_INDEX,
-				CUSTOM_DELIM_INDEX + 1);
-	}
-	
-	private int sumNumbersInDelimitedString(String s, String delimiter) {
-		int sum = 0;
-		String[] numbers = s.split(delimiter);
-		for (String number : numbers) {
-			sum += Integer.parseInt(number.trim());
-		}
-		return sum;
-	}
+    private boolean hasCustomDelimiter(String s) {
+        return s.matches(CUSTOM_DELIMITER_PATTERN);
+    }
+
+    private String removeDelimiterSpecifierFromString(String s) {
+        s = s.substring(CUSTOM_DELIMITED_STRING_INDEX);
+        return s;
+    }
+
+    private String getCustomDelimiter(String s) {
+        return s.substring(CUSTOM_DELIM_INDEX, CUSTOM_DELIM_INDEX + 1);
+    }
+
+    private int sumNumbersInDelimitedString(String s, String delimiter)
+            throws Exception {
+        int sum = 0;
+        String[] numbers = s.split(delimiter);
+        for (String numberStr : numbers) {
+            int n = Integer.parseInt(numberStr.trim());
+            if (n < 0) {
+                throw new Exception("negatives not allowed");
+            }
+            sum += n;
+        }
+        return sum;
+    }
 }
--- string-calculator/java/stringcalc/StringCalculatorTest.java
+++ string-calculator/java/stringcalc/StringCalculatorTest.java
@@ -1,50 +1,64 @@
 package stringcalc;
 
 import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class StringCalculatorTest {
-	
-	private static final String EMPTY = "";
 
-	private StringCalculator calc;
+    private static final String EMPTY = "";
 
-	@Before
-	public void setUp() throws Exception {
-		calc = new StringCalculator();
-	}
+    private StringCalculator calc;
 
-	@Test
-	public void addReturnsZeroForEmptyString() {
-		assertEquals(0, calc.add(EMPTY)); 
-	}
-	
-	@Test
-	public void addCanSumOneNumber() {
-		assertEquals(10, calc.add("10"));
-	}
-	
-	@Test
-	public void addCanSumTwoNumbers() {
-		assertEquals(20, calc.add("10, 10"));
-	}
-	
-	@Test
-	public void addCanSumManyNumbers() {
-		assertEquals(30, calc.add("10, 10, 10"));
-		assertEquals(50, calc.add("10, 10, 5, 0, 25"));
-	}
-	
-	@Test
-	public void sumsNumbersDelimitedByNewlines() {
-		assertEquals(30, calc.add("10\n10,10"));
-	}
-	
-	@Test
-	public void sumsNumbersSeparatedByCustomDelimiter() {
-		assertEquals(3, calc.add("//;\n1;2"));
-	}
+    @Before
+    public void setUp() throws Exception {
+        calc = new StringCalculator();
+    }
+
+    @Test
+    public void addReturnsZeroForEmptyString() throws Exception {
+        assertEquals(0, calc.add(EMPTY));
+    }
+
+    @Test
+    public void addCanSumOneNumber() throws Exception {
+        assertEquals(10, calc.add("10"));
+    }
+
+    @Test
+    public void addCanSumTwoNumbers() throws Exception {
+        assertEquals(20, calc.add("10, 10"));
+    }
+
+    @Test
+    public void addCanSumManyNumbers() throws Exception {
+        assertEquals(30, calc.add("10, 10, 10"));
+        assertEquals(50, calc.add("10, 10, 5, 0, 25"));
+    }
+
+    @Test
+    public void sumsNumbersDelimitedByNewlines() throws Exception {
+        assertEquals(30, calc.add("10\n10,10"));
+    }
+
+    @Test
+    public void sumsNumbersSeparatedByCustomDelimiter() throws Exception {
+        assertEquals(3, calc.add("//;\n1;2"));
+    }
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void throwsAnExceptionWhenAttemptingToAddNegativeNumbers()
+            throws Exception {
+        exception.expect(Exception.class);
+        exception.expectMessage(containsString("negatives not allowed"));
+        calc.add("-1");
+    }
 
 }
