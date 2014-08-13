b2cfbb3d6f308fd9d061111f04928ddee1ff2e0e
Hanif Jameel <hanif.jameel@btinternet.com>
2014-08-13 14:04:36 +0100

    Add first unit test

--- GildedRose/src/test/java/GildedRoseTest.java
+++ GildedRose/src/test/java/GildedRoseTest.java
@@ -1,11 +1,20 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 public class GildedRoseTest {
 
 	@Test
-	public void testTheTruth() {
-		assertTrue(true);
+	public void should_not_add_any_items() {
+		List<Item> items = new ArrayList<Item>();
+		
+		GildedRose.updateQuality(items);
+		
+		assertEquals(0, items.size());
 	}
 }
