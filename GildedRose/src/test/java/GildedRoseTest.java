bea371284f6adf036e696567af6167571174c07d
Hanif Jameel <hanif.jameel@btinternet.com>
2014-08-21 16:45:19 +0100

    Reformat files

--- GildedRose/src/test/java/GildedRoseTest.java
+++ GildedRose/src/test/java/GildedRoseTest.java
@@ -8,10 +8,10 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
-	@Test
-	public void should_not_add_any_items() {
-		List<Item> emptyListOfItems = new LinkedList<Item>();
-		GildedRose.updateQuality(emptyListOfItems);
-		assertThat(emptyListOfItems.size(), is(0));
-	}
+    @Test
+    public void should_not_add_any_items() {
+        List<Item> emptyListOfItems = new LinkedList<Item>();
+        GildedRose.updateQuality(emptyListOfItems);
+        assertThat(emptyListOfItems.size(), is(0));
+    }
 }
