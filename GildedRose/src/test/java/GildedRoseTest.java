fff393b008851888803f5a393b41535ace3678fb
Hanif Jameel <hanif.jameel@btinternet.com>
2014-08-21 16:43:48 +0100

    Test that the list remains the same size

--- GildedRose/src/test/java/GildedRoseTest.java
+++ GildedRose/src/test/java/GildedRoseTest.java
@@ -1,11 +1,17 @@
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.LinkedList;
 
 import org.junit.Test;
 
 public class GildedRoseTest {
 
 	@Test
-	public void testTheTruth() {
-		assertTrue(true);
+	public void should_not_add_any_items() {
+		List<Item> emptyListOfItems = new LinkedList<Item>();
+		GildedRose.updateQuality(emptyListOfItems);
+		assertThat(emptyListOfItems.size(), is(0));
 	}
 }
