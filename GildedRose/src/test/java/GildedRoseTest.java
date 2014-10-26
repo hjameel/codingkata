a92527d6dd252598c6575892d7072085a6102ba8
Hanif Jameel <hanif.jameel@btinternet.com>
2014-10-26 17:54:16 +0000

    Revert "Test that the list remains the same size"
    This reverts commit fff393b008851888803f5a393b41535ace3678fb.

--- GildedRose/src/test/java/GildedRoseTest.java
+++ GildedRose/src/test/java/GildedRoseTest.java
@@ -1,17 +1,11 @@
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
-
-import java.util.List;
-import java.util.LinkedList;
 
 import org.junit.Test;
 
 public class GildedRoseTest {
 
 	@Test
-	public void should_not_add_any_items() {
-		List<Item> emptyListOfItems = new LinkedList<Item>();
-		GildedRose.updateQuality(emptyListOfItems);
-		assertThat(emptyListOfItems.size(), is(0));
+	public void testTheTruth() {
+		assertTrue(true);
 	}
 }
