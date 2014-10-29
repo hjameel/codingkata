eda8af4f3d2afb72e73de8f38807c086cb6911a1
Hanif Jameel <hanif.jameel@btinternet.com>
2014-10-29 22:54:51 +0000

    Add unit test fixture.

---
+++ GildedRose/src/GildedRose.Tests/GildedRoseTest.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using System;
+
+namespace GildedRose.Tests
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+        [Test]
+        public void TestCase()
+        {
+            Assert.Fail("Not implemented");
+        }
+    }
+}
