76ae3fde1470c3906fb50999aab7d4d97c6a6f0d
Hanif Jameel <hanif.jameel@btinternet.com>
2015-05-03 16:32:16 +0100

    Split the main getScore method into smaller ones.

--- Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
+++ Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
@@ -1,7 +1,7 @@
 public class TennisGame1 implements TennisGame {
 
-	private int m_score1 = 0;
-	private int m_score2 = 0;
+	private int player1CurrentPoints = 0;
+	private int player2CurrentPoints = 0;
 	private String player1Name;
 	private String player2Name;
 
@@ -12,64 +12,89 @@ public class TennisGame1 implements TennisGame {
 
 	public void wonPoint(String playerName) {
 		if (playerName.equals(player1Name))
-			m_score1 += 1;
+			player1CurrentPoints++;
 		else if (playerName.equals(player2Name))
-			m_score2 += 1;
+			player2CurrentPoints++;
 	}
 
 	public String getScore() {
 		String score = "";
-		int tempScore = 0;
-		if (m_score1 == m_score2) {
-			switch (m_score1) {
+		if (pointsAreTied()) {
+			score = getScoreForTiedGame();
+		} else if (aPlayerHasScoredFourOrMore()) {
+			score = getScoreForGameWithFourOrMorePoints();
+		} else {
+			score = getScoreForGameWithLessThanFourPoints(score);
+		}
+		return score;
+	}
+
+	private String getScoreForGameWithLessThanFourPoints(String score) {
+		for (int i = 1; i < 3; i++) {
+		    int tempScore = 0;
+			if (i == 1)
+				tempScore = player1CurrentPoints;
+			else {
+				score += "-";
+				tempScore = player2CurrentPoints;
+			}
+			switch (tempScore) {
 			case 0:
-				score = "Love-All";
+				score += "Love";
 				break;
 			case 1:
-				score = "Fifteen-All";
+				score += "Fifteen";
 				break;
 			case 2:
-				score = "Thirty-All";
+				score += "Thirty";
 				break;
-			default:
-				score = "Deuce";
+			case 3:
+				score += "Forty";
 				break;
-
-			}
-		} else if (m_score1 >= 4 || m_score2 >= 4) {
-			int minusResult = m_score1 - m_score2;
-			if (minusResult == 1)
-				score = "Advantage player1";
-			else if (minusResult == -1)
-				score = "Advantage player2";
-			else if (minusResult >= 2)
-				score = "Win for player1";
-			else
-				score = "Win for player2";
-		} else {
-			for (int i = 1; i < 3; i++) {
-				if (i == 1)
-					tempScore = m_score1;
-				else {
-					score += "-";
-					tempScore = m_score2;
-				}
-				switch (tempScore) {
-				case 0:
-					score += "Love";
-					break;
-				case 1:
-					score += "Fifteen";
-					break;
-				case 2:
-					score += "Thirty";
-					break;
-				case 3:
-					score += "Forty";
-					break;
-				}
 			}
 		}
 		return score;
+	}
+
+	private String getScoreForGameWithFourOrMorePoints() {
+		String score;
+		int minusResult = player1CurrentPoints - player2CurrentPoints;
+		if (minusResult == 1)
+			score = "Advantage player1";
+		else if (minusResult == -1)
+			score = "Advantage player2";
+		else if (minusResult >= 2)
+			score = "Win for player1";
+		else
+			score = "Win for player2";
+		return score;
+	}
+
+	private String getScoreForTiedGame() {
+		String score;
+		switch (player1CurrentPoints) {
+		case 0:
+			score = "Love-All";
+			break;
+		case 1:
+			score = "Fifteen-All";
+			break;
+		case 2:
+			score = "Thirty-All";
+			break;
+		default:
+			score = "Deuce";
+			break;
+
+		}
+		return score;
+	}
+
+	private boolean pointsAreTied() {
+		return player1CurrentPoints == player2CurrentPoints;
+	}
+
+	private boolean aPlayerHasScoredFourOrMore() {
+		return player1CurrentPoints >= 4 || player2CurrentPoints >= 4;
 	}
 }
