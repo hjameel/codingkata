54590d39efaa240cf7e2f77dace1f1b7630b9d81
Hanif Jameel <hanif.jameel@btinternet.com>
2015-05-24 18:15:12 +0100

    Refactory to state/strategy pattern.

--- Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
+++ Tennis-Refactoring-Kata/java/src/main/java/TennisGame1.java
@@ -1,8 +1,5 @@
 public class TennisGame1 implements TennisGame {
     private static final int LOVE = 0;
-    private static final int FIFTEEN = 1;
-    private static final int THIRTY = 2;
-    public static final int FORTY = 3;
     private int player1CurrentPoints = LOVE;
     private int player2CurrentPoints = LOVE;
     private String player1Name;
@@ -21,21 +18,44 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        switch (getState()) {
-            case tied:
-                return getScoreForTiedGame();
-            case beyondDeuceBoundary:
-                return getScoreForGamePassedTheDeuceBoundary();
-            default:
-                return getScoreForGameUnderDeuceBoundary();
+        return TennisGame1State.currentState(player1CurrentPoints, player2CurrentPoints).getScore();
+    }
+}
+
+abstract class TennisGame1State {
+    protected static final int LOVE = 0;
+    protected static final int FIFTEEN = 1;
+    protected static final int THIRTY = 2;
+    protected static final int FORTY = 3;
+    protected final int player1CurrentPoints;
+    protected final int player2CurrentPoints;
+
+    public TennisGame1State(int player1CurrentPoints, int player2CurrentPoints) {
+        this.player1CurrentPoints = player1CurrentPoints;
+        this.player2CurrentPoints = player2CurrentPoints;
+    }
+
+    public static TennisGame1State currentState(int player1CurrentPoints, int player2CurrentPoints) {
+        if (player1CurrentPoints == player2CurrentPoints) {
+            return new Tied(player1CurrentPoints, player2CurrentPoints);
+        } else if (player1CurrentPoints > FORTY || player2CurrentPoints > FORTY) {
+            return new BeyondDeuceBoundary(player1CurrentPoints, player2CurrentPoints);
+        } else {
+            return new UnderDeuceBoundary(player1CurrentPoints, player2CurrentPoints);
         }
     }
 
-    private GameState getState() {
-        return TennisGame1State.newState(player1CurrentPoints, player2CurrentPoints).getGameState();
+    abstract String getScore();
+}
+
+class Tied extends TennisGame1State {
+
+    public Tied(int player1CurrentPoints, int player2CurrentPoints) {
+        super(player1CurrentPoints, player2CurrentPoints);
     }
 
-    private String getScoreForTiedGame() {
+    @Override
+    protected String getScore() {
         switch (player1CurrentPoints) {
             case LOVE:
                 return "Love-All";
@@ -47,19 +67,15 @@ public class TennisGame1 implements TennisGame {
                 return "Deuce";
         }
     }
+}
 
-    private String getScoreForGamePassedTheDeuceBoundary() {
-        if (pointsDifference() == 1)
-            return "Advantage player1";
-        else if (pointsDifference() == -1)
-            return "Advantage player2";
-        else if (pointsDifference() >= 2)
-            return "Win for player1";
-        else
-            return "Win for player2";
+class UnderDeuceBoundary extends TennisGame1State {
+    public UnderDeuceBoundary(int player1CurrentPoints, int player2CurrentPoints) {
+        super(player1CurrentPoints, player2CurrentPoints);
     }
 
-    private String getScoreForGameUnderDeuceBoundary() {
+    @Override
+    protected String getScore() {
         return getScoreFromPoints(player1CurrentPoints) + "-"
                 + getScoreFromPoints(player2CurrentPoints);
     }
@@ -78,55 +94,27 @@ public class TennisGame1 implements TennisGame {
                 return "";
         }
     }
-
-    private int pointsDifference() {
-        return player1CurrentPoints - player2CurrentPoints;
-    }
-}
-
-enum GameState {
-    tied,
-    underDeuceBoundary,
-    beyondDeuceBoundary
-}
-
-abstract class TennisGame1State {
-    abstract GameState getGameState();
-
-    public static TennisGame1State newState(int player1CurrentPoints, int player2CurrentPoints) {
-        if (player1CurrentPoints == player2CurrentPoints) {
-            return new Tied();
-        } else if (player1CurrentPoints > TennisGame1.FORTY || player2CurrentPoints > TennisGame1.FORTY) {
-            return new BeyondDeuceBoundary();
-        } else {
-            return new UnderDeuceBoundary();
-        }
-    }
-}
-
-class Tied extends TennisGame1State {
-
-    @Override
-    GameState getGameState() {
-        return GameState.tied;
-    }
-
-}
-
-class UnderDeuceBoundary extends TennisGame1State {
-
-    @Override
-    GameState getGameState() {
-        return GameState.underDeuceBoundary;
-    }
-
 }
 
 class BeyondDeuceBoundary extends TennisGame1State {
 
-    @Override
-    GameState getGameState() {
-        return GameState.beyondDeuceBoundary;
+    public BeyondDeuceBoundary(int player1CurrentPoints, int player2CurrentPoints) {
+        super(player1CurrentPoints, player2CurrentPoints);
     }
 
+    @Override
+    protected String getScore() {
+        if (pointsDifference() == 1)
+            return "Advantage player1";
+        else if (pointsDifference() == -1)
+            return "Advantage player2";
+        else if (pointsDifference() >= 2)
+            return "Win for player1";
+        else
+            return "Win for player2";
+    }
+
+    private int pointsDifference() {
+        return player1CurrentPoints - player2CurrentPoints;
+    }
 }
