5a8c752e9f2645ec29dd0f0cc18ac1b23c2c0736
Hanif Jameel <hanif.jameel@btinternet.com>
2015-05-26 21:53:23 +0100

    Initial commit of c# string calculator

---
+++ string-calculator/csharp/stringcalculator/stringcalculator.Test/StringCalculatorTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace Stringcalculator.Test
+{
+    public class StringCalculatorTest
+    {
+        [TestFixture]
+        public class TheAddMethodShould
+        {
+            [Test]
+            public void Return0WhenGivenAnEmptyString()
+            {
+                Assert.That(StringCalculator.Add(string.Empty), Is.EqualTo(0));
+            }
+        }
+    }
+}
---
+++ string-calculator/csharp/stringcalculator/stringcalculator/StringCalculator.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Stringcalculator
+{
+    public class StringCalculator
+    {
+        public static long Add(string input)
+        {
+            return 0;
+        }
+
+        public static void Main()
+        {
+        }
+    }
+}
