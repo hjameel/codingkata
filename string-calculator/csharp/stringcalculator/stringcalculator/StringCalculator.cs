b1e874ff59af9e67214f1ed5dfea1cded9c76634
Hanif Jameel <hanif.jameel@btinternet.com>
2015-05-26 22:36:55 +0100

    Refactor the string calculator

--- string-calculator/csharp/stringcalculator/stringcalculator/StringCalculator.cs
+++ string-calculator/csharp/stringcalculator/stringcalculator/StringCalculator.cs
@@ -1,17 +1,25 @@
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Stringcalculator
 {
     public class StringCalculator
     {
-        public static long Add(string input)
+        public static long Add(string csv)
         {
-            if (input == string.Empty)
-            {
-                return 0;
-            }
+            return IsEmpty(csv) 
+                ? 0 
+                : TheIndividualElementsOfThe(csv).Sum();
+        }
 
-            return input.Split('\n', ',').Select(long.Parse).Sum();
+        private static bool IsEmpty(string csv)
+        {
+            return csv == string.Empty;
+        }
+
+        private static IEnumerable<long> TheIndividualElementsOfThe(string numbers)
+        {
+            return numbers.Split('\n', ',').Select(long.Parse);
         }
 
         public static void Main()
